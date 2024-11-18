fa33b94b389b958d001030826b2d61063b713477
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2024-11-18 12:54:47 -0500

    Refactor code

--- src/field-error/script.js
+++ src/field-error/script.js
@@ -1,34 +1,38 @@
+// ==
+// HELPER FUNCTIONS
+// ==
+
 function getBetterCustomValidityMessage(fieldEl, inputEl) {
-  if (inputEl.validity.customError) {
-    return;
-  }
-  if (inputEl.validity.badInput) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.patternMismatch) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.rangeOverflow) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.rangeUnderflow) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.stepMismatch) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.tooLong) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.tooShort) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.typeMismatch) {
-    return; // CUSTOMIZE
-  }
-  if (inputEl.validity.valueMissing) {
-    return; // CUSTOMIZE
-  }
+  // if (inputEl.validity.customError) {
+  //   return;
+  // }
+  // if (inputEl.validity.badInput) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.patternMismatch) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.rangeOverflow) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.rangeUnderflow) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.stepMismatch) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.tooLong) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.tooShort) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.typeMismatch) {
+  //   return; // CUSTOMIZE
+  // }
+  // if (inputEl.validity.valueMissing) {
+  //   return; // CUSTOMIZE
+  // }
 }
 
 function setupValidity(fieldEl, inputEl) {
@@ -37,7 +41,6 @@ function setupValidity(fieldEl, inputEl) {
   }
 
   fieldEl.classList.contains('field-error') && fieldEl.classList.remove('field-error');
-  inputEl.validity.customError && inputEl.setCustomValidity('');
 
   if (fieldEl.customValidityValidations) {
     for (const getCustomValidityMessage of fieldEl.customValidityValidations) {
@@ -50,7 +53,12 @@ function setupValidity(fieldEl, inputEl) {
   }
 
   const message = getBetterCustomValidityMessage(fieldEl, inputEl);
-  message && inputEl.setCustomValidity(message);
+  if (message) {
+    inputEl.setCustomValidity(message);
+    return;
+  }
+
+  inputEl.validity.customError && inputEl.setCustomValidity('');
 };
 
 function handleInvalid(fieldEl, inputEl, errorEl) {
@@ -62,6 +70,48 @@ function handleInvalid(fieldEl, inputEl, errorEl) {
   !fieldEl.classList.contains('field-error') && fieldEl.classList.add('field-error');
 }
 
+// ==
+// METHOD OVERRIDE
+// ==
+
+function inputCheckValidity(...args) {
+  this.setupValidity();
+  return this.originalCheckValidity(...args);
+}
+
+function inputReportValidity(...args) {
+  this.setupValidity();
+  return this.originalReportValidity(...args);
+};
+
+function formReportValidity(...args) {
+  for (const inputEl of this.elements) {
+    inputEl.setupValidity && inputEl.setupValidity();
+  }
+  const isValid = this.originalReportValidity(...args);
+  if (!isValid) {
+    // UNCOMMENT WHEN PREVENTING BROWSER ERROR MESSAGE POP UP ON SUBMIT
+    // this.querySelector('.field-error input, .field-error select, .field-error textarea').focus();
+  }
+  return isValid;
+};
+
+// ==
+// EVENT LISTENER
+// ==
+
+function formSubmitEventListener(event) {
+  if (!this.reportValidity()) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    return;
+  }
+};
+
+// ==
+// ADD FIELD VALIDATION
+// ==
+
 export function addFieldValidation(fieldEl) {
   const errorEl = fieldEl.querySelector('.field-error-text');
 
@@ -71,22 +121,13 @@ export function addFieldValidation(fieldEl) {
     };
 
     inputEl.originalCheckValidity = inputEl.originalCheckValidity || inputEl.checkValidity;
-    inputEl.checkValidity = (...args) => {
-      inputEl.setupValidity();
-      return inputEl.originalCheckValidity(...args);
-    };
+    inputEl.checkValidity = inputCheckValidity;
 
     inputEl.originalReportValidity = inputEl.originalReportValidity || inputEl.reportValidity;
-    inputEl.reportValidity = (...args) => {
-      inputEl.setupValidity();
-      return inputEl.originalReportValidity(...args);
-    };
+    inputEl.reportValidity = inputReportValidity;
 
-    inputEl.inputEventListener = () => void inputEl.checkValidity();
-    inputEl.addEventListener('input', inputEl.inputEventListener);
-
-    inputEl.changeEventListener = () => void inputEl.checkValidity();
-    inputEl.addEventListener('change', inputEl.changeEventListener);
+    inputEl.addEventListener('input', inputCheckValidity);
+    inputEl.addEventListener('change', inputCheckValidity);
 
     inputEl.invalidEventListener = (event) => {
       // UNCOMMENT WHEN PREVENTING BROWSER ERROR MESSAGE POP UP ON SUBMIT
@@ -99,6 +140,10 @@ export function addFieldValidation(fieldEl) {
   return fieldEl;
 }
 
+// ==
+// REMOVE FIELD VALIDATION
+// ==
+
 export function removeFieldValidation(fieldEl) {
   for (const inputEl of fieldEl.querySelectorAll('input, select, textarea')) {
     if (inputEl.invalidEventListener) {
@@ -106,15 +151,8 @@ export function removeFieldValidation(fieldEl) {
       delete inputEl.invalidEventListener;
     }
 
-    if (inputEl.inputEventListener) {
-      inputEl.removeEventListener('input', inputEl.inputEventListener);
-      delete inputEl.inputEventListener;
-    }
-
-    if (inputEl.changeEventListener) {
-      inputEl.removeEventListener('change', inputEl.changeEventListener);
-      delete inputEl.changeEventListener;
-    }
+    inputEl.removeEventListener('input', inputCheckValidity);
+    inputEl.removeEventListener('change', inputCheckValidity);
 
     if (inputEl.originalCheckValidity) {
       inputEl.checkValidity = inputEl.originalCheckValidity;
@@ -132,31 +170,18 @@ export function removeFieldValidation(fieldEl) {
   return fieldEl;
 }
 
+// ==
+// ADD FORM VALIDATION
+// ==
+
 export function addFormValidation(formEl) {
   formEl.originalReportValidity = formEl.originalReportValidity || formEl.reportValidity;
-  formEl.reportValidity = (...args) => {
-    for (const elementEl of formEl.elements) {
-      elementEl.setupValidity && elementEl.setupValidity();
-    }
-    const isValid = formEl.originalReportValidity(...args);
-    if (!isValid) {
-      // UNCOMMENT WHEN PREVENTING BROWSER ERROR MESSAGE POP UP ON SUBMIT
-      // formEl.querySelector('.field-error input, .field-error select, .field-error textarea').focus();
-    }
-    return isValid;
-  };
+  formEl.reportValidity = formReportValidity;
 
-  formEl.hasNovalidate = formEl.hasAttribute('novalidate');
-  !formEl.hasNovalidate && formEl.setAttribute('novalidate', '');
+  formEl.hasNoValidate = formEl.hasAttribute('novalidate');
+  !formEl.hasNoValidate && formEl.setAttribute('novalidate', '');
 
-  formEl.submitEventListener = (event) => {
-    if (!formEl.reportValidity()) {
-      event.preventDefault();
-      event.stopImmediatePropagation();
-      return;
-    }
-  };
-  formEl.addEventListener('submit', formEl.submitEventListener);
+  formEl.addEventListener('submit', formSubmitEventListener);
 
   for (const fieldEl of formEl.querySelectorAll('.field')) {
     addFieldValidation(fieldEl);
@@ -165,17 +190,21 @@ export function addFormValidation(formEl) {
   return formEl;
 }
 
+// ==
+// REMOVE FORM VALIDATION
+// ==
+
 export function removeFormValidation(formEl) {
   for (const fieldEl of formEl.querySelectorAll('.field')) {
     removeFieldValidation(fieldEl);
   }
 
-  if (formEl.submitEventListener) {
-    formEl.removeEventListener('submit', formEl.submitEventListener);
-    delete formEl.submitEventListener;
-  }
+  formEl.removeEventListener('submit', formSubmitEventListener);
 
-  !formEl.hasNovalidate && formEl.hasAttribute('novalidate') && formEl.removeAttribute('novalidate');
+  if (formEl.hasNoValidate !== undefined) {
+    !formEl.hasNoValidate && formEl.hasAttribute('novalidate') && formEl.removeAttribute('novalidate');
+    delete formEl.hasNoValidate;
+  }
 
   if (formEl.originalReportValidity) {
     formEl.reportValidity = formEl.originalReportValidity;
