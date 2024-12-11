e6abd9f70f444eb6b5287054796580c8115e25e8
James Robert Huggins Ngo <jamesroberthugginsngo@gmail.com>
2024-12-11 11:23:16 -0500

    Move field error to field and create form validation code

--- src/field-error/README.md
+++
@@ -1,5 +0,0 @@
-# Field Error
-
-## Reference
-
-- https://developer.mozilla.org/en-US/docs/Learn/Forms/Form_validation#validating_forms_using_javascript
---
+++ src/form-validation/script.js
@@ -0,0 +1,260 @@
+function getBetterCustomValidityMessage(inputEl) {
+  // if (inputEl.validity.customError) {
+  //   return;
+  // }
+  // if (inputEl.validity.badInput) {
+  //   return;
+  // }
+  // if (inputEl.validity.patternMismatch) {
+  //   return;
+  // }
+  // if (inputEl.validity.rangeOverflow) {
+  //   return;
+  // }
+  // if (inputEl.validity.rangeUnderflow) {
+  //   return;
+  // }
+  // if (inputEl.validity.stepMismatch) {
+  //   return;
+  // }
+  // if (inputEl.validity.tooLong) {
+  //   return;
+  // }
+  // if (inputEl.validity.tooShort) {
+  //   return;
+  // }
+  // if (inputEl.validity.typeMismatch) {
+  //   return;
+  // }
+  // if (inputEl.validity.valueMissing) {
+  //   return;
+  // }
+}
+
+// ==
+// METHOD OVERRIDES
+// ==
+
+function inputCheckValidity(...args) {
+  const { setup } = this.formValidation;
+  setup();
+
+  return this.constructor.prototype.checkValidity.call(this, ...args);
+}
+
+function inputReportValidity(...args) {
+  const { setup } = this.formValidation;
+  setup();
+
+  return this.constructor.prototype.reportValidity.call(this, ...args);
+};
+
+// ==
+// EVENT LISTENERS
+// ==
+
+function inputInputEventListener(event) {
+  inputCheckValidity.call(this);
+};
+
+function inputInvalidEventListener(event) {
+  // UNCOMMENT WHEN PREVENTING BROWSER ERROR MESSAGE POP UP ON SUBMIT
+  // THIS PREVENTS THE POPUP
+  event.preventDefault();
+
+  if (!this.willValidate) {
+    return;
+  }
+
+  const { errorEl, fieldEl } = this.formValidation;
+
+  if (errorEl) {
+    errorEl.textContent = this.validationMessage;
+  }
+
+  fieldEl.classList.add('field-error');
+};
+
+// ==
+// INPUT FORM VALIDATION FUNCTIONS
+// ==
+
+function inputFormValidationReset(inputEl) {
+  const { fieldEl } = inputEl.formValidation;
+  fieldEl.classList.remove('field-error');
+
+  inputEl.validity.customError && inputEl.setCustomValidity('');
+}
+
+function inputFormValidationSetup(inputEl) {
+  if (!inputEl.willValidate) {
+    return;
+  }
+
+  const { validators, reset } = inputEl.formValidation;
+
+  reset();
+
+  if (validators) {
+    for (const getCustomValidityMessage of validators) {
+      const message = getCustomValidityMessage(inputEl);
+      if (message) {
+        inputEl.setCustomValidity(message);
+        return;
+      }
+    }
+  }
+
+  const message = getBetterCustomValidityMessage(inputEl);
+  if (message) {
+    inputEl.setCustomValidity(message);
+    return;
+  }
+}
+
+function inputFormValidationRemove(inputEl) {
+  inputEl.removeEventListener('input', inputInputEventListener);
+  inputEl.removeEventListener('invalid', inputInvalidEventListener);
+
+  inputEl.checkValidity = inputEl.constructor.prototype.checkValidity;
+  inputEl.reportValidity = inputEl.constructor.prototype.reportValidity;
+
+  const { reset } = inputEl.formValidation;
+  reset();
+
+  delete inputEl.formValidation;
+}
+
+// ==
+// ADD INPUT VALIDATION
+// ==
+
+export function addInputValidation(inputEl) {
+  if (!inputEl.willValidate || inputEl.formValidation) {
+    return;
+  }
+
+  const fieldEl = inputEl.closest('.field');
+  if (!fieldEl) {
+    return;
+  }
+
+  inputEl.formValidation = {
+    fieldEl,
+    errorEl: fieldEl.querySelector('.field-error-text'),
+    reset() {
+      inputFormValidationReset(inputEl);
+    },
+    setup() {
+      inputFormValidationSetup(inputEl);
+    },
+    remove() {
+      inputFormValidationRemove(inputEl);
+    }
+  };
+
+  inputEl.checkValidity = inputCheckValidity;
+  inputEl.reportValidity = inputReportValidity;
+
+  inputEl.addEventListener('input', inputInputEventListener);
+  inputEl.addEventListener('invalid', inputInvalidEventListener);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+function formSetup(formEl) {
+  for (const inputEl of formEl.elements) {
+    inputEl.formValidation?.setup?.()
+  }
+}
+
+// ==
+// METHOD OVERRIDE
+// ==
+
+function formCheckValidity(...args) {
+  formSetup(this);
+  return this.constructor.prototype.checkValidity.call(this, ...args);
+};
+
+function formReportValidity(...args) {
+  formSetup(this);
+
+  const isValid = this.constructor.prototype.reportValidity.call(this, ...args);
+
+  // UNCOMMENT WHEN PREVENTING BROWSER ERROR MESSAGE POP UP ON SUBMIT
+  // THIS RESTORE FOCUS FUNCTIONALITY
+  if (!isValid) {
+    this.querySelector(':invalid')?.focus();
+  }
+
+  return isValid;
+};
+
+// ==
+// EVENT LISTENERS
+// ==
+
+function formResetEventListener(event) {
+  for (const inputEl of this.elements) {
+    inputEl.formValidation?.reset?.()
+  }
+};
+
+function formSubmitEventListener(event) {
+  if (!this.reportValidity()) {
+    event.preventDefault();
+    event.stopImmediatePropagation();
+    return;
+  }
+};
+
+// ==
+// FORM FORM VALIDATION FUNCTIONS
+// ==
+
+function formFormValidationRemove(formEl) {
+  for (const inputEl of formEl.elements) {
+    inputEl.formValidation?.remove?.()
+  }
+
+  formEl.removeEventListener('reset', formResetEventListener);
+  formEl.removeEventListener('submit', formSubmitEventListener);
+
+  formEl.checkValidity = formEl.constructor.prototype.checkValidity;
+  formEl.reportValidity = formEl.constructor.prototype.reportValidity;
+
+  hasAttributeNoValidate === false && formEl.removeAttribute('novalidate');
+
+  delete formEl.formValidation;
+}
+
+// ==
+// ADD FORM VALIDATION
+// ==
+
+export default function addFormValidation(formEl) {
+  if (formEl.formValidation) {
+    return;
+  }
+
+  const hasAttributeNoValidate = formEl.hasAttribute('novalidate');
+
+  formEl.formValidation = {
+    remove() {
+      formFormValidationRemove(formEl);
+    }
+  };
+
+  formEl.setAttribute('novalidate', '');
+
+  formEl.checkValidity = formCheckValidity
+  formEl.reportValidity = formReportValidity;
+
+  formEl.addEventListener('reset', formResetEventListener);
+  formEl.addEventListener('submit', formSubmitEventListener);
+
+  for (const inputEl of formEl.elements) {
+    addInputValidation(inputEl);
+  }
+}
